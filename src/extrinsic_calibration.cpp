831624dfd5993fafcbf1949bb2c06c912e7e8498
Mitchell Scott <miscott@uw.edu>
2019-02-27 09:50:52 -0800

    Updating tof pointcloud publisher

--- src/extrinsic_calibration.cpp
+++ src/extrinsic_calibration.cpp
@@ -2,50 +2,50 @@
 
 int main(int argc, char** argv)
 {
-  //Init ROS
-  ros::init(argc, argv, "image_converter");
-  ros::NodeHandle n;
+    //Init ROS
+    ros::init(argc, argv, "image_converter");
+    ros::NodeHandle n;
 
-  //Get ROS caera info
-  std::string camera_info;
-  if (n.getParam("/color_pointcloud/image_info", camera_info))
-  {
+    //Get ROS caera info
+    std::string camera_info;
+    if (n.getParam("/color_pointcloud/image_info", camera_info))
+    {
     ROS_INFO("Camera info topic subscribed: %s", camera_info.c_str());
-  }
-  else{
+    }
+    else{
     ROS_ERROR("No info topic specified, subscribing to /camera/rgb/camera_info");
     camera_info = "/camera/rgb/camera_info";
-  }
+    }
 
-  std::string image_topic;
-  if (n.getParam("/color_pointcloud/image", image_topic))
-  {
+    std::string image_topic;
+    if (n.getParam("/color_pointcloud/image", image_topic))
+    {
     ROS_INFO("RGB Camera topic subscribed: %s", image_topic.c_str());
-  }
-  else{
+    }
+    else{
     ROS_ERROR("No image topic specified, subscribing to  /camera/rgb/image_color");
     image_topic = "/camera/rgb/image_color";
-  }
+    }
 
-  std::string pointcloud;
-  if (n.getParam("/color_pointcloud/points", pointcloud))
-  {
+    std::string pointcloud;
+    if (n.getParam("/color_pointcloud/points", pointcloud))
+    {
     ROS_INFO("RGB pointcloud topic subscribed: %s", pointcloud.c_str());
-  }
-  else{
+    }
+    else{
     ROS_ERROR("No pointcloud topic specified, subscribed to /camera/depth/points");
     pointcloud = "/camera/depth/points";
-  }
+    }
 
-  //Create salibration obect and subscribe to ROS topics
-  ExtrinsicCalibration ec(image_topic);
-  ros::Subscriber info_sub = n.subscribe(camera_info, 1000,
+    //Create salibration obect and subscribe to ROS topics
+    ExtrinsicCalibration ec(image_topic);
+    ros::Subscriber info_sub = n.subscribe(camera_info, 1000,
                               &ExtrinsicCalibration::img_info_callback, &ec);
-  ros::Subscriber pointcloud_sub = n.subscribe(pointcloud, 1000,
+    ros::Subscriber pointcloud_sub = n.subscribe(pointcloud, 1000,
                               &ExtrinsicCalibration::pointcloud_callback, &ec);
 
-  //Run main function
-  ec.run();
+    //Run main function
+    ec.run();
 
-  return 0;
+    return 0;
 }
--- src/tof_pointcloud_publisher.cpp
+++ src/tof_pointcloud_publisher.cpp
@@ -31,15 +31,14 @@ typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 typedef pcl::PointXYZI PointT;
 
 int main(int argc, char** argv){
-  //init ROS
-  ros::init(argc, argv, "TOF_pointcloud_publisher",
-                                          ros::init_options::NoSigintHandler);
-  ros::NodeHandle nh;
+      //init ROS
+      ros::init(argc, argv, "TOF_pointcloud_publisher",
+                                              ros::init_options::NoSigintHandler);
+      ros::NodeHandle nh("tof");
 
-  ros::Rate loop_rate(30);
-  ros::Publisher tofPub = nh.advertise<PointCloud> ("tof/points/raw", 1);
-  while (ros::ok()){
-  	  Voxel::logger.setDefaultLogLevel(LOG_INFO);
+      ros::Rate loop_rate(30);
+      ros::Publisher tofPub = nh.advertise<PointCloud> ("points_raw", 1);
+      Voxel::logger.setDefaultLogLevel(LOG_INFO);
       //Create camera objects
       CameraSystem sys;
       DepthCameraPtr depthCamera;
@@ -56,101 +55,103 @@ int main(int argc, char** argv){
       }
       if (!depthCamera){
           std::cerr << "SimplePCLViewer: Could not open a depth camera."
-                                                                  << std::endl;
+                                                                    << std::endl;
           return -1;
       }
       std::cout << "Successfully loaded depth camera for device "
-                                              << depthCamera->id() << std::endl;
-      int frame_count = 100;
-      Voxel::TimeStampType last_time_stamp = 0;
+                                                << depthCamera->id() << std::endl;
+      //Set either pointcloud or depth
       std::string type = "pointcloud";
-      //TOF type should be taw, if so step into callback function
-      if (type == "pointcloud"){
-        	 depthCamera->registerCallback(DepthCamera::FRAME_XYZI_POINT_CLOUD_FRAME, [&](DepthCamera &dc, const Frame &frame, DepthCamera::FrameType c) {
-           const XYZIPointCloudFrame *d = dynamic_cast<const XYZIPointCloudFrame *>(&frame);
-           if(!d)
-           {
-             std::cout << "Null frame captured? or not of type XYZIPointCloudFrame" << std::endl;
-             return -1;
-           }
-           std::cout << "Capture frame " << d->id << "@" << d->timestamp;
-           //Print FPS
-           if(last_time_stamp != 0){
-             std::cout << " (" << 1E6/(d->timestamp - last_time_stamp)
-                                                            << " fps)";
-           }
-           std::cout << std::endl;
-           //Create new pointcloud
-           PointCloud::Ptr msg (new PointCloud);
-           msg->header.frame_id = "map";
-           //Loop through all data
-           float data_size = d->size();
-           msg->points.resize(data_size);
-           float int_max = d->points.at(0).i;
-           for (int i=0;i<data_size; i++){
-             float intensity = d->points.at(i).i;
-             if (intensity > int_max){
-               int_max = intensity;
-             }
-           }
-           float z_max = d->points.at(0).z;
-           for (int i = 0; i< data_size; i++){
-             //Add points to pointcloud topic
-             float intensity = d->points.at(i).i;
-             float z = d->points.at(i).z;
-             if (z > 0.3){
-               float x = d->points.at(i).x;
-               float y = d->points.at(i).y;
-               if (z > z_max){
-                 z_max = z;
+
+    while (ros::ok()){
+          Voxel::TimeStampType last_time_stamp = 0;
+          //TOF type should be taw, if so step into callback function
+          if (type == "pointcloud"){
+                     depthCamera->registerCallback(DepthCamera::FRAME_XYZI_POINT_CLOUD_FRAME, [&](DepthCamera &dc, const Frame &frame, DepthCamera::FrameType c) {
+               const XYZIPointCloudFrame *d = dynamic_cast<const XYZIPointCloudFrame *>(&frame);
+               if(!d)
+               {
+                 std::cout << "Null frame captured? or not of type XYZIPointCloudFrame" << std::endl;
+                 return -1;
                }
-               msg->points[i].x = x;
-               msg->points[i].y = y;
-               msg->points[i].z = z;
-               msg->points[i].intensity = intensity;
-             }
-            }
-            tofPub.publish (msg);
-            last_time_stamp = d->timestamp;
-            ros::spinOnce();
-        	});
-        }
-      else if (type == "depth"){
-          depthCamera->registerCallback(DepthCamera::FRAME_DEPTH_FRAME,
-            [&](DepthCamera &dc, const Frame &frame, DepthCamera::FrameType c) {
-            const DepthFrame *d = dynamic_cast<const DepthFrame *>(&frame);
-
-            if (!d)
-            {
-              std::cout << "Null frame captured? or not of type DepthFrame"
-                                                                << std::endl;
-              return;
-            }
-
-            std::cout << "Capture frame" << d->id << "@" << d->timestamp;
-
-            if (last_time_stamp != 0)
-              std::cout << " (" << 1E6 / (d->timestamp - last_time_stamp)
+               std::cout << "Capture frame " << d->id << "@" << d->timestamp;
+               //Print FPS
+               if(last_time_stamp != 0){
+                 std::cout << " (" << 1E6/(d->timestamp - last_time_stamp)
                                                                 << " fps)";
+               }
+               std::cout << std::endl;
+               //Create new pointcloud
+               PointCloud::Ptr msg (new PointCloud);
+               msg->header.frame_id = "map";
+               //Loop through all data
+               float data_size = d->size();
+               msg->points.resize(data_size);
+               float int_max = d->points.at(0).i;
+               for (int i=0;i<data_size; i++){
+                 float intensity = d->points.at(i).i;
+                 if (intensity > int_max){
+                   int_max = intensity;
+                 }
+               }
+               float z_max = d->points.at(0).z;
+               for (int i = 0; i< data_size; i++){
+                 //Add points to pointcloud topic
+                 float intensity = d->points.at(i).i;
+                 float z = d->points.at(i).z;
+                 if (z > 0.3){
+                   float x = d->points.at(i).x;
+                   float y = d->points.at(i).y;
+                   if (z > z_max){
+                     z_max = z;
+                   }
+                   msg->points[i].x = x;
+                   msg->points[i].y = y;
+                   msg->points[i].z = z;
+                   msg->points[i].intensity = intensity;
+                 }
+                }
+                tofPub.publish (msg);
+                last_time_stamp = d->timestamp;
+                ros::spinOnce();
+                    });
+            }
+          else if (type == "depth"){
+              depthCamera->registerCallback(DepthCamera::FRAME_DEPTH_FRAME,
+                [&](DepthCamera &dc, const Frame &frame, DepthCamera::FrameType c) {
+                const DepthFrame *d = dynamic_cast<const DepthFrame *>(&frame);
 
-            std::cout << std::endl;
+                if (!d)
+                {
+                  std::cout << "Null frame captured? or not of type DepthFrame"
+                                                                    << std::endl;
+                  return;
+                }
 
-            last_time_stamp = d->timestamp;
-          });
-        }
+                std::cout << "Capture frame" << d->id << "@" << d->timestamp;
+
+                if (last_time_stamp != 0)
+                  std::cout << " (" << 1E6 / (d->timestamp - last_time_stamp)
+                                                                    << " fps)";
+
+                std::cout << std::endl;
+
+                last_time_stamp = d->timestamp;
+              });
+            }
 
 
-        if(depthCamera->start()){
-          FrameRate r;
-          if(depthCamera->getFrameRate(r))
-            logger(LOG_INFO) << "Capturing at a frame rate of "
-                                    << r.getFrameRate() << " fps" << std::endl;
-          depthCamera->wait();
-        }
-        else{
-          logger(LOG_ERROR) << "Could not start the depth camera "
-                                            << depthCamera->id() << std::endl;
-        }
-      }
+            if(depthCamera->start()){
+              FrameRate r;
+              if(depthCamera->getFrameRate(r))
+                logger(LOG_INFO) << "Capturing at a frame rate of "
+                                        << r.getFrameRate() << " fps" << std::endl;
+              depthCamera->wait();
+            }
+            else{
+              logger(LOG_ERROR) << "Could not start the depth camera "
+                                                << depthCamera->id() << std::endl;
+            }
+          }
       return 0;
 }
